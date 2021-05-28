de7c4d196e25fb720363857e8870f4b3214bd034
VAMMR <42310216+vammr@users.noreply.github.com>
2021-05-28 21:56:49 +0400

    Main file of Lab B

---
+++ Main_labB.m
@@ -0,0 +1,6 @@
+%-------------------Lab B -------------------------------------------%
+% Author : Awabullah Syed 
+% Date : 28th May 2021
+% DESCRIPTION: Identify a set of controller gains that will meet the
+%   prefeerences of the Chief Engineer and consider trade-off in case the
+%   preferences cannot be satisfied and resolve the design problem. 
