3e90263b493cc16a6090c19614c3991f2a3edca4
VAMMR <42310216+vammr@users.noreply.github.com>
2021-05-29 19:35:15 +0400

    Initializing population

--- Main_labB.m
+++ Main_labB.m
@@ -21,23 +21,33 @@ Z = optimizeControlSystem(P);% Re-evaluate the design - post-processed
 %---------------------Calculating Fitness (NSGA-II)-----------------------%
 % Non-dominated Sorting 
 desired_goal = [1 -6 60 -30 2 10 10 8 20 1]; 
+min_range = [0 -20 -30 0 0 0 0 0 1];
+max_range = [1 -6 60 2 10 10 8 20 1];
 % Goal Priorities 
-Hard = 1; 
+Hard = 0.9; 
 High = 0.8;
-Medium = 0.5;
+Moderate = 0.5;
 Low = 0; 
 priority = [Hard High High High Moderate Low Moderate Low Low Moderate];
+
 metric = Hypervolume_MEX(P);
+
+number_of_objectives = 10; % number of decision variables 
+
 n = 100; %Number of iterations 
 [J,distinct_d] = jd(X_sobol,2); %Euclidean p = 2
-for i = 1:n
-    
-end
+% for i = 1:n
+%     
+% end
 
 nond_rank = rank_nds(Z);
 %Crowding Distance 
 crowding_d = crowding(Z,nond_rank); 
 
+% for k = 1:10
+%     min_range(k) = 0.05
+%     max_range(k) = 
+% end
 %---------------------------Selection-for-Variation----------------------%
 
 
