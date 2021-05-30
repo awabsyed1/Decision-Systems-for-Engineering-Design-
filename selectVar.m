3b314abe059b1ef495c480593844778872ee3e56
VAMMR <42310216+vammr@users.noreply.github.com>
2021-05-30 19:44:28 +0400

    implementing new approach

--- selectVar.m
+++ selectVar.m
@@ -2,7 +2,7 @@
 function [ndrank,fitness,crowdingd] = selectVar(P)
     % calculate fitness for NSGA-II optimiser
     % Non-dominance sorting
-    ndrank = rank_nds(P);
+    [ndrank] = rank_nds(P);
     fitness = max(ndrank)-ndrank;
     % Crowding distance
     crowdingd = crowding(P,ndrank);
